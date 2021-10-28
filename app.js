1c8278837bda7532466836596c1163d913fc8579
Yuvraj Singh <ysgaur9919@gmail.com>
2021-10-28 21:44:06 +0530

    added morgan module and added error handeling

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ app.use(morgan("dev"));
 app.use("/users", userRoutes);
 
 app.use((req, res, next) => {
-  res.status(200).json({
-    message: "Root route accessed",
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    error: error.message,
   });
 });
 
